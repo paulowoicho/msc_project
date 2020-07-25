1e373811842a8c1ce0cd7e78ad82a5ca8d98e044
Paul Owoicho <paul.owoicho@aun.edu.ng>
2020-07-26 00:09:56 +0100

    data processing

--- reformat.py
+++ reformat.py
@@ -1,17 +1,29 @@
 """
-Since json files contain fragemented sentences, combine each fragment to form a
-continuous document.
+- Ran out of memory trying to write data to a dataframe and to a csv file (should have used a VM)
+- Wrote it out to a db file instead and exported it to a csv
+- should be able to import the data into colab as a dataframe straight away
 
 """
 import json
 from pathlib import Path
 import sys
+import sqlite3 
 
 
-# podcast = sys.argv[1]
+connection = sqlite3.connect("podcast_dataset.db")
+crsr = connection.cursor()
 
-def reformat(podcast):
-    transcript = ''
+# SQL command to create a table in the database 
+sql_command = """CREATE TABLE dataset ( 
+episode_id TEXT PRIMARY KEY, 
+transcript TEXT 
+);"""
+
+# execute the statement 
+crsr.execute(sql_command)
+
+def add_to_sql(podcast):
+    transcript = ""
     with open(str(podcast)) as json_file:
         data = json.load(json_file)
         for item in data['results']:
@@ -20,14 +32,23 @@ def reformat(podcast):
             except:
                 pass #not all item with 'alternatives' key contain a 'transcript' key
     
-    f = open('concatenated/'+ Path(podcast).stem + '.txt', "a")
-    f.write(transcript)
-    f.close()
+    episode_id = "spotify:episode:" + Path(podcast).stem
+    
+
+    sql_command = "INSERT INTO dataset (episode_id, transcript) VALUES (?, ?);"
+    vals = (episode_id, transcript)
+    crsr.execute(sql_command, vals)
 
 
-rootdir = Path('C:\\Users\\Owoicho\\Desktop\\testes\\spotify-podcasts-2020')
-# Return a list of regular files only, not directories
-file_list = [f for f in rootdir.glob('**/*') if f.is_file()]
+def podcasts_to_df(directory_path):
+  rootdir = Path(directory_path)
+  file_list = [f for f in rootdir.glob('**/*') if f.is_file()]
 
-for file in file_list:
-    reformat(file)
+
+
+  for file in file_list:
+    add_to_sql(file)
+
+podcasts_to_df('spotify-podcasts-2020')
+connection.commit()
+connection.close()
